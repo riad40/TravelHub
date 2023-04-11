45194761ce3c1dbf7e905cbbace779577b94dc7e
riad40 <riadabdelaziz40@gmail.com>
2023-04-11 23:12:04 +0000

    make the logout functionality

--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -1,11 +1,29 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
+import useAuth from "../../hooks/useAuth"
 
 const NavBar = ({ activeTab }: { activeTab: string }): JSX.Element => {
     const [active, setActive] = useState("home")
 
     const handleActive = (tab: string) => {
         setActive(tab)
+    }
+
+    const { auth, dispatch } = useAuth()
+
+    const navigate = useNavigate()
+
+    const handleLogout = async () => {
+        localStorage.clear()
+        dispatch({
+            type: "SET_AUTH",
+            payload: {
+                token: "",
+                user: {},
+            },
+        })
+
+        navigate("/")
     }
 
     useEffect(() => {
@@ -33,12 +51,26 @@ const NavBar = ({ activeTab }: { activeTab: string }): JSX.Element => {
                         <Link to="/destinations">
                             <button className={active === "destinations" ? "bg-blue-400 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400" : "bg-gray-200 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400"}>Destinations</button>
                         </Link>
-                        <Link to="/login">
-                            <button className={active === "login" ? "bg-blue-400 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400" : "bg-gray-200 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400"}>Login</button>
-                        </Link>
-                        <Link to="/register">
-                            <button className={active === "register" ? "bg-blue-400 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400" : "bg-gray-200 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400"}>Register</button>
-                        </Link>
+
+                        {auth?.token ? (
+                            <>
+                                <Link to="/profile">
+                                    <button className={active === "profile" ? "bg-blue-400 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400" : "bg-gray-200 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400"}>Profile</button>
+                                </Link>
+                                <button className="bg-gray-200 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400" onClick={handleLogout}>
+                                    Logout
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/login">
+                                    <button className={active === "login" ? "bg-blue-400 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400" : "bg-gray-200 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400"}>Login</button>
+                                </Link>
+                                <Link to="/register">
+                                    <button className={active === "register" ? "bg-blue-400 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400" : "bg-gray-200 text-gray-600 font-bold py-2 px-4 rounded mr-2 hover:bg-blue-400"}>Register</button>
+                                </Link>
+                            </>
+                        )}
                     </div>
                 </nav>
             </header>
--- frontend/src/state/reducers/authReucers.ts
+++ frontend/src/state/reducers/authReucers.ts
@@ -2,7 +2,7 @@ import { Auth, AuthAction } from "../../@types"
 
 // set initial state
 const initialState: Auth = {
-    user: localStorage.getItem("user") !== undefined || null ? JSON.parse(localStorage.getItem("user") || "") : {},
+    user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user") || "") : {},
     token: localStorage.getItem("token") || "",
 }
 
